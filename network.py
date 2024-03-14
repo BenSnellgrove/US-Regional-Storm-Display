777ca69dd453de16b952f56f2c8be964beed4b16
BenSnellgrove <ben.e.snellgrove@gmail.com>
2024-03-14 14:35:59 +0000

    Change networking to be simpler

--- network.py
+++ network.py
@@ -3,11 +3,4 @@ import network
 station = network.WLAN(network.STA_IF)
 station.active(True)
 
-with open("networks.txt", "r") as networks:
-    for net in networks.read().split("\n"):
-        if not station.isconnected():
-            print(f"Attempting connection to {net.split("\t")[0]}")
-            station.connect(net.split("\t")[0], net.split("\t")[1])
-        else:
-            print("Network found")
-
+station.connect("SSID", "Passkey")
